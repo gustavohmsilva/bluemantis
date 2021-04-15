ac5e020b4c92537a7d5e09699d42a9957e345f5c
Gustavo H. M. Silva <gustavohmsilva@tuta.io>
2021-04-15 23:29:51 +0100

    Started creating the documentation

--- client.go
+++ client.go
@@ -10,12 +10,21 @@ import (
 	"github.com/asaskevich/govalidator"
 )
 
+// Client represents the basic structure that contains the configuration needed
+// for a valid communication with any given Mantis Bug Tracker instance.
+// It should not be created directly, please make use of the function NewClient
+// to create a valid and most importantly, tested, Client.
 type Client struct {
 	*http.Client
 	URL   string
 	Token string
 }
 
+// NewClient creates and test (including the connection) with a MantisBT server
+// and returns a validated pointer to a Client struct for usage in your
+// application. In case of a invalid URL, invalid Token, or communication
+// failure with the MantisBT desired installation, it will return nil and a
+// descriptive error with the problem.
 func NewClient(url, token string) (*Client, error) {
 	if !govalidator.IsURL(url) {
 		return nil, errors.New("invalid URL")
@@ -65,6 +74,9 @@ func testServerConnection(c *Client) error {
 	return nil
 }
 
+// NewIssue will currently still being implemented, at the moment it creates
+// only a basic pointer to a Issue struct, without any of the actual necessary
+// data <- TODO
 func (c *Client) NewIssue() *Issue {
 	issue := new(Issue)
 	issue.Client = c
