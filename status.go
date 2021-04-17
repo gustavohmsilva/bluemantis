0b2c3823474f5c8bd6602b5cb2232d596aacdee7
Gustavo H. M. Silva <gustavohmsilva@tuta.io>
2021-04-17 10:25:33 +0100

    Added more documentation and govalidator tags

--- status.go
+++ status.go
@@ -1,7 +1,10 @@
 package bluemantis
 
+// Rel represent a generic relationship with a different object, such as
+// Categories, Fields, Handlers, Status and etc. It is usually re-labeled to
+// match the object name through the package.
 type Rel struct {
-	ID    uint64
-	Name  string
-	Label string
+	ID    uint64 `valid:"optional"`
+	Name  string `valid:"required"`
+	Label string `valid:"optional"`
 }
