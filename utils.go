6598f3f5a4c82219a3bb76c4f3b3b06700f79e8f
Gustavo H. M. Silva <gustavohmsilva@tuta.io>
2021-04-17 10:25:49 +0100

    New go file destined to store private functions of the package

---
+++ utils.go
@@ -0,0 +1,37 @@
+package bluemantis
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+func testServerConnection(c *Client) error {
+	testRequest, err := http.NewRequest(
+		"GET",
+		fmt.Sprintf("%s%s", c.URL, getMyUserInfo),
+		nil,
+	)
+	if err != nil {
+		return errors.New("problem contacting MantisBT server")
+	}
+	testRequest.Header.Add("Authorization", c.Token)
+	testResponse, err := c.Do(testRequest)
+	if err != nil {
+		return errors.New("problem contacting MantisBT server")
+
+	}
+	if testResponse.StatusCode != 200 {
+		return errors.New("the token isn't valid for this server")
+	}
+	return nil
+}
+
+/*
+func testBaseIssue(bi *BaseIssue) []error {
+	bi.Category
+	bi.Description
+	bi.Project
+	bi.
+}
+*/
