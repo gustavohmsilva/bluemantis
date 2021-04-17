b61b543242a6031218e1d10fb22445125c8b6cde
Gustavo H. M. Silva <gustavohmsilva@tuta.io>
2021-04-17 10:25:10 +0100

    Added GoValidator tags for required and optional content

--- project.go
+++ project.go
@@ -1,11 +1,13 @@
 package bluemantis
 
+// Project represents a instance of a project, as a single MantisBT application
+// can support logging bugs and errors from multiple projects at the same time.
 type Project struct {
-	ID          uint64
-	Name        string
-	Description string
-	Enabled     bool
-	FilePath    string
-	Status      Rel
-	ViewState   Rel
+	ID          uint64 `valid:"optional"`
+	Name        string `valid:"required"`
+	Description string `valid:"optional"`
+	Enabled     bool   `valid:"optional"`
+	FilePath    string `valid:"optional"`
+	Status      Rel    `valid:"-"`
+	ViewState   Rel    `valid:"-"`
 }
