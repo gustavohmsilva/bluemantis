6150a9d2dadc7ce1bf0da12ceaa0abd82deaea9f
Gustavo H. M. Silva <gustavohmsilva@tuta.io>
2021-04-17 10:22:59 +0100

    created tests for function NewIssue

--- client_test.go
+++ client_test.go
@@ -1,6 +1,7 @@
 package bluemantis
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"reflect"
@@ -101,6 +102,153 @@ func TestNewClient(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("NewClient() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestClient_NewIssue(t *testing.T) {
+	type fields struct {
+		Client *http.Client
+		URL    string
+		Token  string
+	}
+	type args struct {
+		bascInfo *BaseIssue
+	}
+
+	validIssueClient := fields{
+		Client: &http.Client{},
+		URL:    "http://test.local/",
+		Token:  "SAMPLETOKEN",
+	}
+
+	baseRequest, _ := http.NewRequest(
+		"POST",
+		fmt.Sprintf("%s%s", validIssueClient.URL, newIssue),
+		nil,
+	)
+	baseRequest.Header.Add("Content-Type", "application/json")
+	baseRequest.Header.Add("Authorization", validIssueClient.Token)
+
+	validIssueBasicInfo := args{
+		bascInfo: &BaseIssue{
+			Summary:     "This is the summary",
+			Description: "This is the description",
+			Category: &Category{
+				Name: "This is the Category Name",
+			},
+			Project: &Project{
+				Name: "This is the Project Name",
+			},
+		},
+	}
+
+	MissingSummaryBasicInfo := args{
+		bascInfo: &BaseIssue{
+			Description: "This is the description",
+			Category: &Category{
+				Name: "This is the Category Name",
+			},
+			Project: &Project{
+				Name: "This is the Project Name",
+			},
+		},
+	}
+
+	MissingDescriptionBasicInfo := args{
+		bascInfo: &BaseIssue{
+			Summary: "This is the summary",
+			Category: &Category{
+				Name: "This is the Category Name",
+			},
+			Project: &Project{
+				Name: "This is the Project Name",
+			},
+		},
+	}
+
+	MissingCategoryBasicInfo := args{
+		bascInfo: &BaseIssue{
+			Summary:     "This is the summary",
+			Description: "This is the description",
+			Project: &Project{
+				Name: "This is the Project Name",
+			},
+		},
+	}
+
+	MissingProjectBasicInfo := args{
+		bascInfo: &BaseIssue{
+			Summary:     "This is the summary",
+			Description: "This is the description",
+			Category: &Category{
+				Name: "This is the Category Name",
+			},
+		},
+	}
+
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   *Issue
+	}{
+		{
+			"Valid Issue test",
+			validIssueClient,
+			validIssueBasicInfo,
+			&Issue{
+				Client: &Client{
+					Client: &http.Client{},
+					URL:    validIssueClient.URL,
+					Token:  validIssueClient.Token,
+				},
+				requestResponse: requestResponse{
+					request: baseRequest,
+				},
+				BaseIssue: &BaseIssue{
+					Summary:     "This is the summary",
+					Description: "This is the description",
+					Category: &Category{
+						Name: "This is the Category Name",
+					},
+					Project: &Project{
+						Name: "This is the Project Name",
+					},
+				},
+			},
+		}, {
+			"Missing Summary test",
+			validIssueClient,
+			MissingSummaryBasicInfo,
+			nil,
+		}, {
+			"Missing Description test",
+			validIssueClient,
+			MissingDescriptionBasicInfo,
+			nil,
+		}, {
+			"Missing Category test",
+			validIssueClient,
+			MissingCategoryBasicInfo,
+			nil,
+		}, {
+			"Missing Project test",
+			validIssueClient,
+			MissingProjectBasicInfo,
+			nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Client{
+				Client: tt.fields.Client,
+				URL:    tt.fields.URL,
+				Token:  tt.fields.Token,
+			}
+			if got := c.NewIssue(tt.args.bascInfo); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Client.NewIssue() = %v, want %v", got, tt.want)
 			}
 		})
 	}
